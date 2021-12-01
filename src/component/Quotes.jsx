14cb3a16fd6aefcf4eb06690c09cdd9c0203a99c
Nahid <diegonahid@gmail.com>
2021-12-01 13:17:27 +0600

    quote button modified

--- src/component/Quotes.jsx
+++ src/component/Quotes.jsx
@@ -1,13 +1,13 @@
 const Quotes = ({ handleClick, quoteText, author }) => {
   return (
     <div className="contaier">
-      <button className="btn btn-primary" onClick={handleClick}>
-        Generate Quotes
-      </button>
       <p className="text-muted">{quoteText}</p>
       <cite>
         <blockquote> {author}</blockquote>
-      </cite>
+      </cite>{" "}
+      <button className="btn btn-primary" onClick={handleClick}>
+        Generate Quotes
+      </button>
     </div>
   );
 };
