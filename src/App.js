d36b5c53964ea574056201293d989641eaabc5ca
Nahid <diegonahid@gmail.com>
2021-12-07 01:39:44 +0600

    cssTransition added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Quotes from "./component/Quotes";
 import React, { useReducer, useEffect, useRef } from "react";
 import useFetch from "./component/customHooks/CustomFetch";
 import reducer from "./reducer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import About from "./component/About";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
   const initialstate = {
     quoteText: "When you get tired,learn to rest not quit",
     author: "Banksy",
@@ -37,41 +40,55 @@ function App() {
     <div>
       <Navbar />
       <div className="App-container">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                {state.isloading && (
-                  <h3>
-                    Quotes for you is
-                    <div className="loading loading08">
-                      <span>L</span>
-                      <span>O</span>
-                      <span>A</span>
-                      <span>D</span>
-                      <span>I</span>
-                      <span>N</span>
-                      <span>G</span>
-                    </div>
-                  </h3>
-                )}
-                {!state.isloading && (
-                  <Quotes
-                    ref={animRef}
-                    quoteText={state.quoteText}
-                    author={state.author}
-                    handleClick={handleClick}
-                    isloading={state.isloading}
-                  />
-                )}
-              </div>
-            }
-          />
-          <Route path="/about" element={<About />}>
-            <Route path="bd" element={<div>bangladesh is my motherland</div>} />
-          </Route>
-        </Routes>
+        <TransitionGroup>
+          <CSSTransition
+            exit={false}
+            key={location.pathname}
+            timeout={5000}
+            mountOnEnter={true}
+            unmountOnExit={true}
+            classNames="test"
+          >
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <div>
+                    {state.isloading && (
+                      <h3>
+                        Quotes for you is
+                        <div className="loading loading08">
+                          <span>L</span>
+                          <span>O</span>
+                          <span>A</span>
+                          <span>D</span>
+                          <span>I</span>
+                          <span>N</span>
+                          <span>G</span>
+                        </div>
+                      </h3>
+                    )}
+                    {!state.isloading && (
+                      <Quotes
+                        ref={animRef}
+                        quoteText={state.quoteText}
+                        author={state.author}
+                        handleClick={handleClick}
+                        isloading={state.isloading}
+                      />
+                    )}
+                  </div>
+                }
+              />
+              <Route path="/about" element={<About />}>
+                <Route
+                  path="bd"
+                  element={<div>bangladesh is my motherland</div>}
+                />
+              </Route>
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     </div>
   );
--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 const About = () => {
+  const [isActive, setIsActive] = useState(false);
+  const handleZoom = () => {
+    setIsActive(true);
+  };
   const styles = {
     color: "white",
     backgroundColor: "black",
@@ -8,14 +14,28 @@ const About = () => {
   const navigate = useNavigate();
   return (
     <div className="about">
-      <p className="text-muted" style={{ ...styles }}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae,
-        temporibus!
-      </p>
-      <button className="btn btn-primary" onClick={() => navigate("/")}>
-        back to home
-      </button>
-      <Outlet />
+      <CSSTransition
+        in={isActive}
+        timeout={500}
+        classNames="zoom"
+        exit={true}
+        onEntered={(props) => setIsActive(false)}
+      >
+        <div>
+          <p className="text-muted" style={{ ...styles }}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae,
+            temporibus!
+          </p>
+          <button className="btn btn-primary" onClick={() => navigate("/")}>
+            back to home
+          </button>
+
+          <Outlet />
+        </div>
+      </CSSTransition>
+      <br />
+
+      <button onClick={handleZoom}>Zoom IN</button>
     </div>
   );
 };
